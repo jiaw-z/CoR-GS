648700bd519ee0bc9bc865217e1dd370c21499b7
jiaw-z <1060794126@qq.com>
2024-07-17 14:27:06 +0800

    dtu scripts

--- scripts/run_dtu.sh
+++ scripts/run_dtu.sh
@@ -6,7 +6,6 @@ workspace=$3
 python train.py \
 --source_path $dataset -m $workspace \
 --eval  -r 4 --n_views 3 \
---random_background \
 --iterations 10000 --position_lr_max_steps 10000 \
 --densify_until_iter 10000 \
 --densify_grad_threshold 0.0005 \
